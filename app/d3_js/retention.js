5f85e6ce788bf5b12d4acb7f88ee758cc0071fa8
tedballin <tedz@live.cn>
2019-06-11 20:50:11 -0700

    Retention section: adds dual axis, dcr img

--- app/d3_js/retention.js
+++ app/d3_js/retention.js
@@ -3,7 +3,7 @@ let retention_margin = {
     top: 10,
     right: 50,
     bottom: 50,
-    left: 50
+    left: 30
 };
 
 //height and width
@@ -12,14 +12,19 @@ let retention_width = 600 - retention_margin.left - retention_margin.right;
 
 //scales
 let retention_xScale = d3.scalePoint().range([0, retention_width]).padding(0.5);
-let retention_yScale = d3.scaleLinear().range([retention_height, 0]);
+let retention_yScale_dist = d3.scaleLinear().range([retention_height, 0]);
+let retention_yScale_prov = d3.scaleLinear().range([retention_height, 0]);
 
 //axes
 let retention_xAxis = d3.axisBottom()
     .scale(retention_xScale);
 
-let retention_yAxis = d3.axisLeft()
-    .scale(retention_yScale)
+let retention_yAxis_dist = d3.axisLeft()
+    .scale(retention_yScale_dist)
+    .tickSizeOuter(0);
+
+let retention_yAxis_prov = d3.axisRight()
+    .scale(retention_yScale_prov)
     .tickSizeOuter(0);
 
 //lines
@@ -28,7 +33,7 @@ let prov_line = d3.line()
         return retention_xScale(d.SCHOOL_YEAR);
     })
     .y(function (d) {
-        return retention_yScale(d.PROV_NET_RETENTION);
+        return retention_yScale_prov(d.PROV_NET_RETENTION);
     })
     .curve(d3.curveMonotoneX); //smooth the line
 
@@ -38,35 +43,46 @@ let dist_line = d3.line()
         return retention_xScale(d.SCHOOL_YEAR);
     })
     .y(function (d) {
-        return retention_yScale(d.DIST_NET_RETENTION);
+        return retention_yScale_dist(d.DIST_NET_RETENTION);
     })
     .curve(d3.curveMonotoneX); //smooth the line
 
 //canvas
 let retention_svg = d3.select('#retention_container')
     .append('svg')
-    .attr("preserveAspectRatio", "xMinYMin meet") // This forces uniform scaling for both the x and y, aligning the midpoint of the SVG object with the midpoint of the container element.
-    .attr("viewBox", "0 0 600 400") //define the aspect ratio, the inner scaling of object lengths and coordinates
+    .attr('preserveAspectRatio', 'xMinYMin meet') // This forces uniform scaling for both the x and y, aligning the midpoint of the SVG object with the midpoint of the container element.
+    .attr('viewBox', '0 0 600 400') //define the aspect ratio, the inner scaling of object lengths and coordinates
     .attr('class', 'svg-content');
 
 let retention_chartGroup = retention_svg.append('g')
     .attr('class', 'chartGroup')
-    .attr("transform", "translate(" + retention_margin.left + "," + retention_margin.top + ")");
+    .attr('transform', 'translate(' + retention_margin.left + ',' + retention_margin.top + ')');
 
-let retention_legend = d3.select('#retention_control')
+let retention_legend = d3.select('#retention_control .row')
     .append('svg')
-    .append('g')
-    .attr('class', 'legendContainer');
+    .attr('class', 'legendContainer col-4')
+    .append('g');
+
+console.log(sd_arr);
+//populate dropdown menu
+for (let i = 0; i < sd_arr.length; i++) {
+    let opt = sd_arr[i];
+    d3.select('#retention_distDropdown .list')
+        .append('div')
+        .text(opt)
+        //take the sd number (first 4 letters) as value
+        .attr('data-value', opt.substring(0, 4));
+};
 
 // function retentionClear() {
 //     //clear existing line
-//     let existingPath = d3.selectAll("#demoContainer .demo_line");
+//     let existingPath = d3.selectAll('#demoContainer .demo_line');
 //     existingPath.exit();
 //     existingPath.transition()
 //         .duration(500)
 //         .remove();
 //     //clear existing legends
-//     let existingLegend = d3.selectAll("#demo-legend .legend");
+//     let existingLegend = d3.selectAll('#demo-legend .legend');
 //     console.log(existingLegend);
 //     existingLegend.exit();
 //     existingLegend.transition()
@@ -74,9 +90,9 @@ let retention_legend = d3.select('#retention_control')
 //         .remove();
 
 //     //clear tooltips 
-//     demo_chartGroup.selectAll(".demott_circle")
+//     demo_chartGroup.selectAll('.demott_circle')
 //         .remove();
-//     d3.selectAll(".demott_rect")
+//     d3.selectAll('.demott_rect')
 //         .remove();
 // }
 
@@ -85,11 +101,12 @@ d3.csv('../assets/raw_data/retention_provincial.csv', function (error, data) {
         throw error;
     }
 
-    let prov_net_retention = data;
-    prov_net_retention.forEach(function (d) {
+    let provData = data;
+    provData.forEach(function (d) {
+        d.SCHOOL_YEAR = (parseDate(d.SCHOOL_YEAR)).getFullYear()
         d.PROV_NET_RETENTION = +d.PROV_NET_RETENTION;
     })
-    console.log(prov_net_retention);
+
 
 
     function retentionUpdate(dist) {
@@ -104,24 +121,30 @@ d3.csv('../assets/raw_data/retention_provincial.csv', function (error, data) {
             });
 
             districtData.forEach(function (d) {
+                d.SCHOOL_YEAR = (parseDate(d.SCHOOL_YEAR)).getFullYear();
                 d.DIST_NET_RETENTION = +d.DIST_NET_RETENTION;
             });
 
+            console.log(districtData);
             //set scale domain
             retention_xScale.domain(districtData.map(function (d) {
                 return d.SCHOOL_YEAR;
             }));
 
-            retention_yScale.domain([Math.min(prov_net_retention),
-                Math.max(Math.max(prov_net_retention), d3.max(districtData, function (d) {
-                    return d.dist_net_retention;
-                }))
-            ]);
+            retention_yScale_prov.domain([d3.min(provData, function (d) {
+                return d.PROV_NET_RETENTION;
+            }), 0]);
+
+            retention_yScale_dist.domain([d3.min(districtData, function (d) {
+                return d.DIST_NET_RETENTION;
+            }), d3.max(districtData, function (d) {
+                return d.DIST_NET_RETENTION;
+            })]);
 
 
             //draw liens
             let prov_retention_line = retention_chartGroup.append('path')
-                .datum(prov_net_retention)
+                .datum(provData)
                 .attr('class', 'prov_retention_line')
                 .attr('d', prov_line)
                 .attr('fill', 'none')
@@ -134,17 +157,46 @@ d3.csv('../assets/raw_data/retention_provincial.csv', function (error, data) {
                 .attr('fill', 'none')
                 .attr('stroke-width', '2');
 
-            // retention_legend.append("rect")
-            //     .attr("x", 10)
-            //     .attr("y", i * 20)
-            //     .attr("width", 18)
-            //     .attr("height", 18)
-            //     .style("fill", '#002663');
+            //animate path
+            let plineLength = prov_retention_line.node().getTotalLength();
+            let dlineLength = dist_retention_line.node().getTotalLength();
 
-            // retention_legend.append("text")
-            //     .attr("x", 30)
-            //     .attr("y", 15 + i * 20)
-            //     .text(selectedDistricts[i]);
+            prov_retention_line.attr("stroke-dasharray", plineLength + " " + plineLength)
+                .attr("stroke-dashoffset", plineLength)
+                .transition()
+                .duration(1500)
+                .attr("stroke-dashoffset", 0);
+
+            dist_retention_line.attr("stroke-dasharray", dlineLength + " " + dlineLength)
+                .attr("stroke-dashoffset", dlineLength)
+                .transition()
+                .duration(1500)
+                .attr("stroke-dashoffset", 0);
+
+            //legends
+            retention_legend.append('rect')
+                .attr('x', 10)
+                .attr('y', 20)
+                .attr('width', 18)
+                .attr('height', 18)
+                .style('fill', '#002663');
+
+            retention_legend.append('text')
+                .attr('x', 30)
+                .attr('y', 35)
+                .text('Province');
+
+            retention_legend.append('rect')
+                .attr('x', 10)
+                .attr('y', 50)
+                .attr('width', 18)
+                .attr('height', 18)
+                .style('fill', '#FCBA19');
+
+            retention_legend.append('text')
+                .attr('x', 30)
+                .attr('y', 65)
+                .text('Southeast Kootenay');
 
             if ($('#retention_container .yAxis').length) {
 
@@ -152,66 +204,78 @@ d3.csv('../assets/raw_data/retention_provincial.csv', function (error, data) {
                 let tran = d3.transition()
                     .duration(1500);
 
-                d3.select("#retention_container .yAxis")
-                    .transition(tran)
-                    .call(retention_xAxis);
-
-                d3.select("#retention_container .xAxis")
+                d3.select('#retention_container .yAxis')
                     .transition(tran)
                     .call(retention_yAxis);
 
-                //grid line
-                d3.selectAll("g.yAxis g.tick")
-                    .append("line")
-                    .attr("class", "gridline")
-                    .attr("x1", 0)
-                    .attr("y1", 0)
-                    .attr("x2", retention_width)
-                    .attr("y2", 0);
+                d3.select('#retention_container .xAxis')
+                    .transition(tran)
+                    .call(retention_xAxis);
 
-                d3.selectAll("g.xAxis g.tick")
-                    .append("line")
-                    .attr("class", "gridline")
-                    .attr("x1", 0)
-                    .attr("y1", -retention_height)
-                    .attr("x2", 0)
-                    .attr("y2", 0);
+                //grid line
+                d3.selectAll('g.yAxis g.tick')
+                    .append('line')
+                    .attr('class', 'gridline')
+                    .attr('x1', 0)
+                    .attr('y1', 0)
+                    .attr('x2', retention_width)
+                    .attr('y2', 0);
+
+                d3.selectAll('g.xAxis g.tick')
+                    .append('line')
+                    .attr('class', 'gridline')
+                    .attr('x1', 0)
+                    .attr('y1', -retention_height)
+                    .attr('x2', 0)
+                    .attr('y2', 0);
 
             } else {
 
                 //axes
                 retention_chartGroup.append('g')
-                    .attr('class', 'yAxis')
-                    .call(retention_yAxis)
-                    .append("text")
-                    .attr("transform", "rotate(-90)")
+                    .attr('class', 'yAxis_dist')
+                    .call(retention_yAxis_dist)
+                    .append('text')
+                    .attr('transform', 'rotate(-90)')
                     .attr('fill', '#4c4c4c')
-                    .attr("y", 6)
-                    .attr("dy", ".8em")
-                    .style("text-anchor", "end")
-                    .text("FTE");
+                    .attr('y', 6)
+                    .attr('dy', '.8em')
+                    .style('text-anchor', 'end')
+                    .text('FTE - District');
+
+                retention_chartGroup.append('g')
+                    .attr('class', 'yAxis_prov')
+                    .attr('transform', 'translate( ' + retention_width + ', 0 )')
+                    .call(retention_yAxis_prov)
+                    .append('text')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('fill', '#4c4c4c')
+                    .attr('y', 6)
+                    .attr('dy', '-0.2em')
+                    .style('text-anchor', 'end')
+                    .text('FTE - Province');
 
                 retention_chartGroup.append('g')
                     .attr('class', 'xAxis')
                     .attr('transform', 'translate(0,' + retention_height + ')')
-                    .call(retention_xAis);
+                    .call(retention_xAxis);
 
                 //grid line
-                d3.selectAll("g.yAxis g.tick")
-                    .append("line")
-                    .attr("class", "gridline")
-                    .attr("x1", 0)
-                    .attr("y1", 0)
-                    .attr("x2", retention_width)
-                    .attr("y2", 0);
+                d3.selectAll('g.yAxis_dist g.tick')
+                    .append('line')
+                    .attr('class', 'gridline')
+                    .attr('x1', 0)
+                    .attr('y1', 0)
+                    .attr('x2', retention_width)
+                    .attr('y2', 0);
 
-                d3.selectAll("g.xAxis g.tick")
-                    .append("line")
-                    .attr("class", "gridline")
-                    .attr("x1", 0)
-                    .attr("y1", -retention_height)
-                    .attr("x2", 0)
-                    .attr("y2", 0);
+                d3.selectAll('g.xAxis_dist g.tick')
+                    .append('line')
+                    .attr('class', 'gridline')
+                    .attr('x1', 0)
+                    .attr('y1', -retention_height)
+                    .attr('x2', 0)
+                    .attr('y2', 0);
 
             }
 
@@ -223,6 +287,21 @@ d3.csv('../assets/raw_data/retention_provincial.csv', function (error, data) {
 
     retentionUpdate('05');
 
+    //dropdown select district
+    //removes event handlers from selected elements as updateGraph
+    $('#retention_distDropdown').unbind().on('click', function (et) {
+        console.log('clicked');
+        $(this).toggleClass('active');
 
+        //or add on click when appending the divs
+        d3.selectAll('retention_distDropdown .list div')
+            .on('click', function () {
+                $('#retention_distDropdown').text($(this).text());
+                $('#retention_distDropdown').attr('attr', 'dropDown');
 
-});
+                //reset target district 
+                targetDistrict = d3.select(this).attr('data-value');
+            });
+        });
+
+    });
