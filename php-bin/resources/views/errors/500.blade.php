5247f18b37e1ebba3c05a6c4d73938d26390fa93
Michael Tang <michael.tang@gov.bc.ca>
2019-09-19 11:07:16 -0700

    added a new 500 page

---
+++ php-bin/resources/views/errors/500.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+
+  <meta charset="utf-8">
+
+  <title>B.C. Education System Performance - 500 Not Found</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+  <style>
+    
+    #hero.error-page #report-title {
+      background: transparent;
+    }
+
+  </style>
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+  <div id="header">
+    <div class="restrain">
+      <div id="logo">
+        <a href="http://www2.gov.bc.ca/"><img src="/img/gov3_bc_logo.png" alt="Government of B.C." title="Government of B.C."></a>
+      </div>
+      <h1 class="visuallyhidden">Enhanced District Reporting</h1>
+    </div>
+  </div><!-- /#header -->
+
+  <div id="secondary-nav" class="contracted"></div>
+
+  <main class="main-content">
+    
+    <div class="aqua-bg fill-viewport error-page" id="hero">
+      <div class="restrain">
+
+        <h2 id="report-title" class="center">503 Error</h2>
+        <p id="tagline">
+          This website is currently offline. You can try refreshing the page.<br><br>
+          If the problem persists feel free to <a href="mailto:EDUCWEBS@Victoria1.gov.bc.ca">contact us</a>.<br><br>
+        </p>
+
+      </div>
+    </div>
+
+  </main>
+
+  <footer id="footer">
+    <div class="restrain">
+
+      <nav>
+        <ul>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home">Home</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/about-gov-bc-ca">About gov.bc.ca</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home/disclaimer">Disclaimer</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home/privacy">Privacy</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home/accessibility">Accessibility</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home/copyright">Copyright</a></li>
+          <li><a href="http://www2.gov.bc.ca/gov/content/home/contact-us">Contact Us</a></li>
+        </ul>
+      </nav>
+
+      <p class="white" style="padding: 2rem 0;">This website uses Google Analytics to track visitor traffic. <a style="text-decoration: underline;" href="http://www.google.com/policies/privacy/partners/">Learn how Google uses data</a> when you use our partner&#039;s sites or apps.<br>For more information, please contact <a style="text-decoration: underline;" href="mailto:educ.systemperformance@gov.bc.ca">educ.systemperformance@gov.bc.ca</a>.</p>
+
+    </div>
+  </footer><!-- /#footer -->
+
+  <script src="/js/bundle.js"></script>
+
+</body>
+</html>
