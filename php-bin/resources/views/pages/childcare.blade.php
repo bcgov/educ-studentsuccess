abd8fc9884a86e423d3c5c259f33fd03fa41e61f
Michael Tang <michael.tang@gov.bc.ca>
2022-11-30 15:17:54 -0800

    child acre

--- php-bin/resources/views/pages/childcare.blade.php
+++ php-bin/resources/views/pages/childcare.blade.php
@@ -7,14 +7,16 @@
     <div class="restrain">
     
       <h2 id="directory-main-heading" class="ministry-blue slide-title">Child Care</h2>
-      <h4 style="color:white">The Ministry of Education and Child Care provides several programs and services to support parents with a variety of quality childcare options and to assist parents and caregivers with the costs associated with childcare.</h4><br><br>
+      <h4 style="color:white">The Ministry of Education and Child Care provides several programs and services to support parents with a variety of quality child care options and to assist parents and caregivers with the costs associated with child care.</h4><br><br>
+      <h4 style="color:white"><a href="https://www2.gov.bc.ca/gov/content/family-social-supports/caring-for-young-children">Learn more about child care in B.C.</a></h4>
+      <br>
     </div>
   </div>
 
   <section class="container">
   <br><br>
     <iframe title="Child Care Student Success - Child Care Operating Funding" width="1500" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiNjZlYjEyOTAtZDM0ZS00MzllLWE5NTItODQ2YTJjNmJjOWIxIiwidCI6IjZmZGI1MjAwLTNkMGQtNGE4YS1iMDM2LWQzNjg1ZTM1OWFkYyJ9" frameborder="0" allowFullScreen="true"></iframe>
-    <div style="background:white;margin-top: -65px;position:absolute;height: 60px;width: 69%;"></div>
+    <div style="background:white;margin-top: -65px;position:absolute;height: 60px;width: 100%;"></div>
   </section>
 
 @endsection
